6c24b127cfc992cd64f6e2446801af42c87f6a5f
1trickPwnyta <sarahpace37@gmail.com>
2024-03-19 12:46:25 -0700

    Update to 1.5

---
+++ 1.5/Source/Patch_StorytellerUI.cs
@@ -0,0 +1,25 @@
+﻿using RimWorld;
+using Verse;
+using HarmonyLib;
+
+namespace UnlimitedThreatScale
+{
+    // Patches the vanilla custom difficulty slider to be a free-entry text box instead
+
+    [HarmonyPatch(typeof(StorytellerUI))]
+    [HarmonyPatch("DrawCustomDifficultySlider")]
+    public static class Patch_StorytellerUI_DrawCustomDifficultySlider
+    {
+        public static bool Prefix(ref Listing_Standard listing, ref string optionName, ref float value)
+        {
+            // If the optionName is threatScale, replace the slider with our widget
+            if (optionName == "threatScale")
+            {
+                UnlimitedThreatScaleSettings.DoThreatScaleWidget(ref listing, ref value);
+                return false;
+			}
+
+            return true;
+        }
+    }
+}
---
+++ 1.5/Source/UnlimitedThreatScaleSettings.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace UnlimitedThreatScale
+{
+    public class UnlimitedThreatScaleSettings : ModSettings
+    {
+        public static bool UncapThreatPoints = false;
+        public static int ThreatPointsCap = 10000;  // Default same as vanilla
+
+        public static void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listingStandard = new Listing_Standard();
+            listingStandard.Begin(inRect);
+
+            if (Find.Storyteller?.difficulty?.threatScale != null)
+            {
+                DoThreatScaleWidget(ref listingStandard, ref Find.Storyteller.difficulty.threatScale);
+            }
+
+            listingStandard.CheckboxLabeled("UnlimitedThreatScale_UncapThreatPoints".Translate(), ref UncapThreatPoints);
+
+            if (!UncapThreatPoints)
+            {
+                listingStandard.Label("UnlimitedThreatScale_ThreatPointsCap".Translate());
+                string buffer = ThreatPointsCap.ToString();
+                listingStandard.IntEntry(ref ThreatPointsCap, ref buffer, 100);
+                ThreatPointsCap = Mathf.Max(35, ThreatPointsCap);
+            }
+
+            listingStandard.End();
+        }
+
+        public static void DoThreatScaleWidget(ref Listing_Standard listing, ref float value)
+        {
+            int num = (int) GenMath.RoundTo(value * 100, 1f);
+            TaggedString label = "Difficulty_ThreatScale_Label".Translate() + ": " + value.ToStringByStyle(ToStringStyle.PercentZero, ToStringNumberSense.Absolute);
+            listing.Label(label, -1f, "Difficulty_ThreatScale_Info".Translate());
+            string buffer = num.ToString();
+            int num2 = num;
+            listing.IntEntry(ref num, ref buffer, 10);
+            if (num2 != num)
+            {
+                value = GenMath.RoundTo(num / 100f, 0.01f);
+            }
+        }
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref UncapThreatPoints, "UncapThreatPoints");
+            Scribe_Values.Look(ref ThreatPointsCap, "ThreatPointsCap");
+        }
+    }
+}
